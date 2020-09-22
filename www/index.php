db5ef398f40ace44b0540ffe2323a47d382a124c
prostoAE <trident998@gmail.com>
2020-09-22 15:02:10 +0300

    #1.2 Эксперт PHP: Функционал загрузки страниц

--- www/index.php
+++ www/index.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+
+// Определяем с каким контроллером будем работать
+$controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'index';
+echo 'Подключаем php файл (Контроллер) = ' . $controllerName . '<br>';
+
+// Определяем с какой функцией будем работать
+$actionName = isset($_GET['action']) ? ucfirst($_GET['action']) : 'index';
+echo 'Функция формирующая страницу (action) = ' . $actionName . '<br>';
+
+// Подключаем контроллер
+include_once '../controllers/' . $controllerName . 'Controller.php';
