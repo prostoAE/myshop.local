44a0aa2055099bd996d51d46948c2142cfdc9add
prostoAE <trident998@gmail.com>
2020-09-23 11:08:52 +0300

    #1.4.2 Эксперт PHP: Шаблонизатор Smarty. Продолжение

--- config/config.php
+++ config/config.php
@@ -8,3 +8,28 @@
 define('PathPrefix', '../controllers/');
 define('PathPostfix', 'controller.php');
 //<
+
+//> Используемый шаблон
+$template = 'default';
+
+// Пути к файлам шаблонов
+define('TemplatePrefix', "../views/{$template}/");
+define('TemplatePostfix', '.tpl');
+
+// Пути у файлам шаблонов в вебпространтсвте
+define('TemplateWebPath', "../templates/{$template}/");
+//<
+
+//> Инициализация шаблонизатора Smarty
+// put full path to Smarty.class.php
+require '../library/Smarty/libs/Smarty.class.php';
+
+$smarty = new Smarty();
+
+$smarty->setTemplateDir(TemplatePrefix);
+$smarty->setCompileDir('../tmp/smarty/templates_c');
+$smarty->setCacheDir('../tmp/smarty/cache');
+$smarty->setConfigDir('../library/Smarty/configs');
+
+$smarty->assign('templateWebPath', TemplateWebPath);
+//<
